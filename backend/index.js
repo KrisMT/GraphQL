4f336f06c48dbe2ad91ec1d6a17b7882b3fd267f
KrisMT <ratiodetector@hotmail.com>
2017-12-12 15:11:14 +0700

    Initial project for GraphQL

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlExpress, graphiqlExpress } = require('apollo-server-express');
+
+const schema = require('./schema');
+const connecMongo = require('./mongo-connector');
+
+const start = async () => {
+  const mongo = await connecMongo();
+
+  var app = express();
+  app.use('/graphql', bodyParser.json(), graphqlExpress({
+    context: {
+      mongo,
+    },
+    schema,
+  }));
+
+  app.use('/graphiql', graphiqlExpress({
+    endpointURL: '/graphql',
+  }));
+
+  const PORT = 3000;
+
+  app.listen(PORT, () => {
+    console.log(`Hackernews GraphQL server running on port ${PORT}.`);
+  });
+}
+
+start();
