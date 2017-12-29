1cefc13807301706bdc1f4c894d9babb8089122b
KrisMT <ratiodetector@hotmail.com>
2017-12-29 21:17:10 +0700

    Modify all query to DataLoader

--- backend/dataloaders.js
+++ backend/dataloaders.js
@@ -1,12 +1,51 @@
 const DataLoader = require('dataloader');
+const _ = require('lodash');
 
 async function batchUsers (Users, keys) {
   return await Users.find({_id: {$in: keys}}).toArray();
 }
 
-module.exports = ({Users}) => ({
+const batchLinks = async (Links, keys) => {
+  return await Links.find({_id: {$in: keys}}).toArray();
+}
+
+const batchVotesBylinkId = async (Votes, keys) => {
+  //find the votes data by linkId and convert to array
+  const documents = await Votes.find({linkId: {$in: keys}}).toArray();
+
+  // grouping the data in array by linkId ==> {'1': [...], '2': [...]}
+  const gs = _.groupBy(documents, 'linkId');
+
+  //order by keys
+  return keys.map(key => (gs[key]? gs[key] : []) );
+}
+
+const batchVotesByuserId = async (Votes, keys) => {
+  //find the votes data by userId and convert to array
+  const documents = await Votes.find({userId: {$in: keys}}).toArray();
+
+  // grouping the data in array by linkId ==> {'1': [...], '2': [...]}
+  const gs = _.groupBy(documents, 'userId');
+
+  //order by keys
+  return keys.map(key => (gs[key]? gs[key] : []) );
+}
+
+module.exports = ({Users, Links, Votes}) => ({
   userLoader: new DataLoader(
     keys => batchUsers(Users, keys),
+    {cacheKeyFn: key => key.toString()},
+  ),
+  linkLoader: new DataLoader(
+    keys => batchLinks(Links, keys),
+    {cacheKeyFn: key => key.toString()},
+  ),
+  voteBylinkIdLoader: new DataLoader(
+    keys => batchVotesBylinkId(Votes, keys),
+    {cacheKeyFn: key => key.toString()},
+  ),
+  voteByuserIdLoader: new DataLoader(
+    keys => batchVotesByuserId(Votes, keys),
     {cacheKeyFn: key => key.toString()},
   ),
 });
--- backend/schema/resolvers.js
+++ backend/schema/resolvers.js
@@ -52,14 +52,14 @@ module.exports = {
     postedBy: async ({postedById}, data, {dataloaders: {userLoader}}) => {
       return await userLoader.load(postedById);
     },
-    votes: async ({_id}, data, {mongo: {Votes}}) => {
-      return await Votes.find({linkId: _id}).toArray();
+    votes: async ({_id}, data, {dataloaders: {voteBylinkIdLoader}}) => {
+      return await voteBylinkIdLoader.load(_id);
     },
   },
   User: {
     id: root => root._id || root.id,
-    votes: async ({_id}, data, {mongo: {Votes}}) => {
-      return await Votes.find({userId: _id}).toArray();
+    votes: async ({_id}, data, {dataloaders: {voteByuserIdLoader}}) => {
+      return await voteByuserIdLoader.load(_id);
     },
   },
   Vote: {
@@ -67,8 +67,8 @@ module.exports = {
     user: async ({userId}, data, {dataloaders: {userLoader}}) => {
       return await userLoader.load(userId);
     },
-    link: async ({linkId}, data, {mongo: {Links}}) => {
-      return await Links.findOne({_id: linkId});
+    link: async ({linkId}, data, {dataloaders: {linkLoader}}) => {
+      return await linkLoader.load(linkId);
     },
   },
 };
