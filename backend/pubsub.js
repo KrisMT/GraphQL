4cc92c884994200d12ba9d23811359b538ecd91e
KrisMT <ratiodetector@hotmail.com>
2018-01-24 09:39:00 +0700

    [CHANGED] the graphql subscription to graphql redis subscription

--- backend/pubsub.js
+++ backend/pubsub.js
@@ -1,3 +1,13 @@
-const { PubSub } = require('graphql-subscriptions');
+const { RedisPubSub } = require('graphql-redis-subscriptions');
 
-module.exports = new PubSub();
+const pubsub = new RedisPubSub({
+  connection: {
+    host: 'redis',
+    port: 6379,
+    retry_strategy: options => {
+      return Math.max(options.attempt * 100, 3000);
+    }
+  }
+});
+
+module.exports = pubsub;
