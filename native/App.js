59705d99ae08e8622ef90f95fb7b7d6ffb23d06b
KrisMT <ratiodetector@hotmail.com>
2018-06-04 14:10:16 +0700

    [ADD] Apollo (GraphQL) to Expo

--- native/App.js
+++ native/App.js
@@ -1,12 +1,68 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
+import { ApolloLink } from 'apollo-link';
+import { ApolloProvider } from 'react-apollo';
+import { Query } from "react-apollo";
+import gql from "graphql-tag";
+
+const client = new ApolloClient({
+  link: ApolloLink.from([
+    onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors)
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      if (networkError) console.log(`[Network error]: ${networkError}`);
+    }),
+    new HttpLink({
+      uri: 'https://w5xlvm3vzz.lp.gql.zone/graphql',
+      credentials: 'same-origin'
+    })
+  ]),
+  cache: new InMemoryCache()
+});
+
+const ExchangeRates = () => (
+  <Query
+    query={gql`
+      {
+        rates(currency: "USD") {
+          currency
+          rate
+        }
+      }
+    `}
+  >
+    {({ loading, error, data }) => {
+      if (loading) return <Text>Loading...</Text>;
+      if (error) return <Text>Error :(</Text>;
+
+      return data.rates.map(({ currency, rate }) => (
+        <Text key={currency}>
+          {`${currency}: ${rate}`}
+        </Text>
+      ));
+    }}
+  </Query>
+);
+
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+      <ApolloProvider client={client} >
+        <View style={styles.container}>
+          <Text>Open up App.js to start working on your app!</Text>
+          <Text>Open up App.js to start working on your app!</Text>
+          <ExchangeRates />
+        </View>
+      </ApolloProvider>
     );
   }
 }
---
+++ native/README.md
@@ -0,0 +1,6 @@
+#Install
+1. install react native cli with 'npm install -g react-native-cli'.
+2. Adding 'Apollo' to project :
+'npm install apollo-client apollo-cache-inmemory apollo-link-http apollo-link-error apollo-link --save'
+3. Linking the 'Apollo' to 'Expo' with 'react-native link'.
+4. Funny with your code.
