64d4fd209d78a8f42fb07f04ee328dd418008f11
KrisMT <ratiodetector@hotmail.com>
2018-06-10 14:00:35 +0700

    [MOD] check store with single page

--- native/components/CheckStoreScreen.js
+++ native/components/CheckStoreScreen.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {
   View,
   Text,
+  Button,
+  StyleSheet,
 } from 'react-native';
 
 import {
@@ -17,6 +19,8 @@ class CheckStoreScreen extends React.Component {
 
   state = {
     hasCameraPermission: null,
+    strButtonTitle: "SCAN Page",
+    bQRPage: false,
   }
 
   async componentWillMount() {
@@ -28,23 +32,49 @@ class CheckStoreScreen extends React.Component {
     alert(`Bar code with type ${type} and data ${data} has been scanned!`);
   }
 
+  _toogleQR = () => {
+    const { bQRPage} = this.state;
+    const strButtonTitle = bQRPage? "SCAN Page" : "SETTING PAGE";
+    this.setState({strButtonTitle});
+    this.setState({bQRPage: !bQRPage});
+  }
+
   render() {
-    const { hasCameraPermission } = this.state;
+    const { hasCameraPermission, bQRPage, strButtonTitle } = this.state;
     if( hasCameraPermission === null) {
       return <Text>Requesting for cameara permission</Text>
-    } else if( hasCameraPermission === false ) {
-      return <Text>Not access to camera</Text>
+        //} else if( hasCameraPermission === false ) {
+        //return <Text>Not access to camera</Text>
+    } else if(bQRPage === false) {
+      return (
+        <View style={styles.container}>
+          <Button title={strButtonTitle} onPress={this._toogleQR} />
+          <View>
+            <Text>Setting anythings</Text>
+          </View>
+        </View>
+      );
     } else {
       return (
-        <View style={{ flex: 1 }}>
-          <BarCodeScanner
-            onBarCodeRead={this._handleBarCodeRead}
-            style={styleSheet.absoluteFill}
-          />
+        <View style={styles.container}>
+          <Button title={strButtonTitle} onPress={this._toogleQR} />
+          <View style={{flex: 1}}>
+            <BarCodeScanner
+              onBarCodeRead={this._handleBarCodeRead}
+              style={StyleSheet.absoluteFill}
+            />
+          </View>
         </View>
       );
     }
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22,
+  },
+});
 
 export default CheckStoreScreen;
